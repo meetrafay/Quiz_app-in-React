5b0ab1de7331a5a64bbcfd8faad57a58e7d6f26c
Abdul Rafay <khanrafay358@gmail.com>
2022-11-17 15:32:16 +0500

    quiz questions updated

--- src/Helpers/QuestionBank.js
+++ src/Helpers/QuestionBank.js
@@ -1,26 +1,42 @@
 export const Question = [
   {
-    prompt: "1+1 is equal to ?",
-    A: "2",
-    B: "5",
-    C: "10",
-    D: "11",
+    prompt: "Who is the father of C Language ?",
+    A: "Bjarne stroustrup",
+    B: "james A.gosling",
+    C: "Dennis Ritchie",
+    D: "Dr. e.f codd",
+    answer : "C"
+  },
+  {
+    prompt: "Which of the following keywords is used to define a variable in javascript?",
+    A: "var",
+    B: "let",
+    C: "both A & B",
+    D: "None of the above",
+    answer : "C"
+  },
+  {
+    prompt: "What keyword is used to check whether a given property is valid or not?",
+    A: "in",
+    B: "is",
+    C: "exist",
+    D: "lies",
     answer : "A"
   },
   {
-    prompt: "1+2 is equal to ?",
-    A: "2",
-    B: "5",
-    C: "10",
-    D: "11",
-    answer : "A"
+    prompt: "What does the javascript 'debugger' statement do?",
+    A: "It will debug all error at runtime",
+    B: "It act as a breakpoint in a program",
+    C: "It will debug error in the current statement if any",
+    D: "All of the above",
+    answer : "B"
   },
   {
-    prompt: "1+3 is equal to ?",
-    A: "2",
-    B: "5",
-    C: "10",
-    D: "11",
+    prompt: "Javascript is _____ Language",
+    A: "Scripting",
+    B: "Machine",
+    C: "Frontend",
+    D: "All of thr above",
     answer : "A"
   },
 ];
